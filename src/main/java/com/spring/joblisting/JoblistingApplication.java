a99002dfc31ba92e78ae91e99a65082318af39da
Akula <sai.akula@cdk.com>
2025-08-20 12:11:11 +0530

    update

--- src/main/java/com/spring/joblisting/JoblistingApplication.java
+++ src/main/java/com/spring/joblisting/JoblistingApplication.java
@@ -2,7 +2,7 @@ package com.spring.joblisting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.*;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
