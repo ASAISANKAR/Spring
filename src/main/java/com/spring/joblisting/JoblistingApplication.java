1fb2bdf0b14b79cee15afb5c9062516759bdf186
Akula <sai.akula@cdk.com>
2025-08-20 12:17:01 +0530

    update

--- src/main/java/com/spring/joblisting/JoblistingApplication.java
+++ src/main/java/com/spring/joblisting/JoblistingApplication.java
@@ -12,7 +12,10 @@ public class JoblistingApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(JoblistingApplication.class, args);
 		System.out.println("Server Running....");
 		Alien t1 = context.getBean(Alien.class);
+		Alien t2 = new Alien();
 		t1.test();
+		System.out.println("From test 2");
+		t2.test();
 	}
 
 }
