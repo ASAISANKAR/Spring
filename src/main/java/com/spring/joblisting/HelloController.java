d15853ed8ed7cb2e8ef6987f2bcf07198c333cea
Akula <sai.akula@cdk.com>
2025-08-21 10:54:22 +0530

    update

--- src/main/java/com/spring/joblisting/HelloController.java
+++ src/main/java/com/spring/joblisting/HelloController.java
@@ -1,10 +1,11 @@
 package com.spring.joblisting;
 
 
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +29,10 @@ public class HelloController {
     {
         return "User ID: "+id;
     }
+
+    @PostMapping("/api/user")
+    public String user(@RequestBody Alien alien){
+        return "User created: "+alien.getId();
+    }
+
 }
